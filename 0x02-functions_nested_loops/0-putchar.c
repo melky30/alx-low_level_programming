fab3bc44c5c6d073985970ed75a5001fd8c9e732
Melchizedek <melkyud@gmail.com>
2022-09-13 11:03:20 -0700

    0.-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,24 @@
 #include "0-putchar.c"
+
 /**
- * main - Prints _putchar
+ * main - entry point
  *
- * Return: Always 0 (Success)
+ * Description: Prints alx with _putchar
+ *
+ * Return: always 0
  */
+
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	int i = 0;
+	char c;
+	char s[] = "_putchar\n";
+
+	while (i <= 9)
+	{
+		c = s[i];
+		_putchar(c);
+		i++;
+	}
 	return (0);
 }
