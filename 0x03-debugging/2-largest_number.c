bcbc88dee3feb29bd7092880681fd7b3e8100e41
Melchizedek <melkyud@gmail.com>
2022-09-16 08:11:24 -0700

    2 large number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,27 +1,57 @@
-#incude "main.h"
-/**
- * largest_number - returns the largest of 3 number
- * @a: first integer
- * @b second integer
- * @b third integer
- * Return: largest number
- */
+#include <stdio.h>                                                  
 
-int largest_number(int a, int b, int c)
+                                                                    
+
+void positive_or_negative(int i);                                   
+
+int largest_number(int , int , int );                               
+
+void print_remaining_days(int, int, int);                           
+
+int convert_day(int, int);                                          
+
+                                                                    
+
+#endif /* MAIN_H */                                                 
+
+.c                                                                  
+
+root@dae97be74fd4:/alx-low_level_programming/0x03-debugging# cat > 2
+
+-largest_number.c                                                   
+
+#include "main.h"                                                   
+
+                                                                    
+
+/**                                                                 
+
+ * largest_number - returns the largest of 3 numbers                
+
+ * @a: first integer                                                
+
+ * @b: second interger                                              
+
+ * @c: third integer                                                
+
+ * Return: largest number                                           
+
+                                                                    
+
+int largest number (int a, int b, int c)
 {
-int largest;
-if (a >= b && a >=c)
+int largest:
+if (aa >= b && a >= c)
 {
 largest = a;
 }
-else if (b >= a && b >=c)
+else if (b >=a && b >= c)
 {
 largest = b;
 }
 else
-{
 largest = c;
 }
-return (largest);
+Return: (largest);
 }
 .c
