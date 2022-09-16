4f07bf613720505637207dd4482d6276fa47224d
Melchizedek <melkyud@gmail.com>
2022-09-16 08:57:43 -0700

    2-largest number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,30 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
-#include "main.h"
-
-/**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
-
-int largest_number(int a, int b, int c)
-{
-    int largest;
-
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-    return (largest);
-}
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+#endif /* MAIN_H */
