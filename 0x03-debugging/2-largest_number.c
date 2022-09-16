888981e80d33c523ceeebb329547ff91bcb5b492
Melchizedek <melkyud@gmail.com>
2022-09-16 08:21:43 -0700

    2 largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,57 +1,28 @@
-#include <stdio.h>                                                  
-
-                                                                    
-
-void positive_or_negative(int i);                                   
-
-int largest_number(int , int , int );                               
-
-void print_remaining_days(int, int, int);                           
-
-int convert_day(int, int);                                          
-
-                                                                    
-
-#endif /* MAIN_H */                                                 
-
-.c                                                                  
-
-root@dae97be74fd4:/alx-low_level_programming/0x03-debugging# cat > 2
-
--largest_number.c                                                   
-
 #include "main.h"                                                   
-
-                                                                    
-
-/**                                                                 
-
- * largest_number - returns the largest of 3 numbers                
-
- * @a: first integer                                                
-
+                                                                                       
+/**                           
+ * largest_number - returns the largest of 3 numbers                                   
+ * @a: first integer                                                         
  * @b: second interger                                              
-
- * @c: third integer                                                
-
+ * @c: third integer                                                                         
  * Return: largest number                                           
-
                                                                     
-
-int largest number (int a, int b, int c)
-{
-int largest:
-if (aa >= b && a >= c)
-{
-largest = a;
-}
-else if (b >=a && b >= c)
-{
-largest = b;
-}
-else
-largest = c;
-}
-Return: (largest);
-}
+                                                                    
+                                                                    
+int largest number (int a, int b, int c)                            
+{                                                                   
+int largest:                                                        
+if (a >= b && a >= c)                                              
+{                                                                   
+largest = a;                                                        
+}                                                                   
+else if (b >=a && b >= c)                                           
+{                                                                   
+largest = b;                                                        
+}                                                                   
+else                                                                
+largest = c;                                                        
+}                                                                   
+Return: (largest);                                                  
+}                                                                   
 .c
