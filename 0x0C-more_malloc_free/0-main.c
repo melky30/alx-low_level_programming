a7eae59975526b806c12a83a9f29dd6d11b99211
Melchizedek <melkyud@gmail.com>
2022-10-06 10:31:26 -0700

    malloc check.c

---
+++ 0x0C-more_malloc_free/0-main.c
@@ -0,0 +1,91 @@
+
+Signed in successfully.
+0x0C. C - More malloc, free
+C
+Memory allocation
+ By: Julien Barbier
+ Weight: 1
+ Project will start Oct 6, 2022 6:00 AM, must end by Oct 7, 2022 6:00 AM
+ was released at Oct 6, 2022 12:00 PM
+ An auto review will be launched at the deadline
+Concepts
+For this project, we expect you to look at this concept:
+
+Automatic and dynamic allocation, malloc and free
+Resources
+Read or watch:
+
+Do I cast the result of malloc?
+man or help:
+
+exit (3)
+calloc
+realloc
+Learning Objectives
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+General
+How to use the exit function
+What are the functions calloc and realloc from the standard library and how to use them
+Copyright - Plagiarism
+You are tasked to come up with solutions for the tasks below yourself to meet with the above learning objectives.
+You will not be able to meet the objectives of this or any following project by copying and pasting someone else’s work.
+You are not allowed to publish any content of this project.
+Any form of plagiarism is strictly forbidden and will result in removal from the program.
+Requirements
+General
+Allowed editors: vi, vim, emacs
+All your files will be compiled on Ubuntu 20.04 LTS using gcc, using the options -Wall -Werror -Wextra -pedantic -std=gnu89
+All your files should end with a new line
+A README.md file, at the root of the folder of the project is mandatory
+Your code should use the Betty style. It will be checked using betty-style.pl and betty-doc.pl
+You are not allowed to use global variables
+No more than 5 functions per file
+The only C standard library functions allowed are malloc, free and exit. Any use of functions like printf, puts, calloc, realloc etc… is forbidden
+You are allowed to use _putchar
+You don’t have to push _putchar.c, we will use our file. If you do it won’t be taken into account
+In the following examples, the main.c files are shown as examples. You can use them to test your functions, but you don’t have to push them to your repo (if you do we won’t take them into account). We will use our own main.c files at compilation. Our main.c files might be different from the one shown in the examples
+The prototypes of all your functions and the prototype of the function _putchar should be included in your header file called main.h
+Don’t forget to push your header file
+Quiz questions
+Great! You've completed the quiz successfully! Keep going! (Show quiz)
+Tasks
+0. Trust no one
+mandatory
+Write a function that allocates memory using malloc.
+
+Prototype: void *malloc_checked(unsigned int b);
+Returns a pointer to the allocated memory
+if malloc fails, the malloc_checked function should cause normal process termination with a status value of 98
+julien@ubuntu:~/0x0b. more malloc, free$ cat 0-main.c
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *c;
+    int *i;
+    float *f;
+    double *d;
+
+    c = malloc_checked(sizeof(char) * 1024);
+    printf("%p\n", (void *)c);
+    i = malloc_checked(sizeof(int) * 402);
+    printf("%p\n", (void *)i);
+    f = malloc_checked(sizeof(float) * 100000000);
+    printf("%p\n", (void *)f);
+    d = malloc_checked(INT_MAX);
+    printf("%p\n", (void *)d);
+    free(c);
+    free(i);
+    free(f);
+    free(d);
+    return (0);
+}
